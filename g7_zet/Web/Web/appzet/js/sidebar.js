c089f053f5bb655fb8b37c278700947baf576483
spedrol2020crn <spedrol@artgarfiques.org>
2021-05-17 14:49:55 +0200

    provam12

---
+++ g7_zet/Web/Web/appzet/js/sidebar.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100px";
+    document.getElementById("main").style.marginLeft = "80px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "-1%";
+  } 
